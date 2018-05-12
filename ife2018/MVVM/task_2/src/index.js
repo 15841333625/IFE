252ca93516252f04effb2376f0fbec4a840de395
anwel <201692095@mail.dlut.edu.cn>
2018-05-12 18:23:27 +0800

    MVVM task2

---
+++ ife2018/MVVM/task_2/src/index.js
@@ -0,0 +1,14 @@
+import san from 'san';
+var MyApp = san.defineComponent({
+    template: '<p>Hello {{name}}!</p>',
+
+    initData: function () {
+        return {
+            name: 'World'
+        };
+    }
+});
+
+
+var myApp = new MyApp();
+myApp.attach(document.body);
---
+++ ife2018/MVVM/task_2/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+      rules: [
+        {
+            test: /.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader" // 将 ES6 编译成 ES5
+            }
+        },
+        {
+            test: /.san$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "san-loader" // 编译 san 语法
+            }
+        },
+        {
+            test: /.css$/,
+            exclude: /node_modules/,
+            use: [ 'style-loader', 'css-loader' ] // 将编译完成的css插入html中
+        }
+      ]
+   }
+};
